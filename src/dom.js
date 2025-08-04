2b966f3e3b90d59fd0f36876abb0135a73585512
Joshua Concepcion <joshuarconcepcion@gmail.com>
2025-08-04 14:31:04 -0700

    Cleaned up html and css, fixed layout of main todo container UI

--- src/dom.js
+++ src/dom.js
@@ -1,10 +1,16 @@
 import todayLogo from './media/images/todayLogo.png';
+import { format } from 'date-fns';
 
-const sidebarLogoContainer = document.querySelector('#sidebar-logo-container');
+const sidebarLogoContainer = document.querySelector('.sidebar-logo-container');
+const dateDisplay = document.getElementById('date-display');
 
 export function renderUI() {
     const sidebarLogo = new Image();
-    sidebarLogo.id = 'sidebar-logo'
+    sidebarLogo.classList.add('sidebar-logo');
     sidebarLogo.src = todayLogo;
     sidebarLogoContainer.appendChild(sidebarLogo);
+
+    const today = new Date();
+    const formattedDate = format(today, 'PPPP');
+    dateDisplay.textContent = formattedDate;
 }
